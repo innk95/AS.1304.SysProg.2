bf3c678a15b716a7108cc68af1bcec456a5b6f81
temermer <temka.fancier@mail.ru>
2016-06-21 19:36:11 +0300

    Update Robot13.cpp

--- Application/Robots/Robot13/Robot13.cpp
+++ Application/Robots/Robot13/Robot13.cpp
@@ -111,7 +111,7 @@ extern "C" __declspec(dllexport) void DoStep(StepInfo* _stepInfo)
 	int maxDistToMove = stepInfo->gameConfig.V_max * myInfo->V * myInfo->E /
 		(stepInfo->gameConfig.L_max * stepInfo->gameConfig.E_max);
 
-	if ((double)myInfo->E / stepInfo->gameConfig.E_max < 0.75 &&
+	if ((double)myInfo->E / stepInfo->gameConfig.E_max < 0.85 &&
 		myInfo->E - stepInfo->gameConfig.dE_V > 0) {
 		Point CstationPos = getCStation();
 		if (Pyth(CstationPos.x, CstationPos.y) <= maxDistToMove) {
@@ -122,7 +122,18 @@ extern "C" __declspec(dllexport) void DoStep(StepInfo* _stepInfo)
 			}
 		}
 		else {
-			stepInfo->pRobotActions->addActionRedistribution(myInfo->L*0.01/*новое значение A*/, myInfo->L*0.79 /*новое значение P*/, myInfo->L*0.20 /* новое значение V*/);
+			if ((myInfo->y = 0) && (myInfo->y = stepInfo->gameConfig.H) && (myInfo->x = 0) && (myInfo->x = stepInfo->gameConfig.W)) { stepInfo->pRobotActions->addActionMove(maxDistToMove*0.3, maxDistToMove*0.3); }
+	
+			if (CstationPos.y > myInfo->y) { stepInfo->pRobotActions->addActionMove(0, -rand() % 5); }
+			else
+			{	stepInfo->pRobotActions->addActionMove(0,  rand() % 5);
+			if (CstationPos.x > myInfo->x) { stepInfo->pRobotActions->addActionMove(-rand() % 5, 0); }
+			else { stepInfo->pRobotActions->addActionMove(rand() % 5, 0); }
+			}
+	        
+			
+		stepInfo->pRobotActions->addActionRedistribution(myInfo->L*0.01/*новое значение A*/, myInfo->L*0.79 /*новое значение P*/, myInfo->L*0.20 /* новое значение V*/);
+	
 		}
 
 	}
@@ -137,13 +148,10 @@ extern "C" __declspec(dllexport) void DoStep(StepInfo* _stepInfo)
 			}
 		}
 		else {
-			Point CstationPos = getCStation();
-			if (Pyth(CstationPos.x, CstationPos.y) <= maxDistToMove) {
-				stepInfo->pRobotActions->addActionMove(CstationPos.x, CstationPos.y);
-			}
-			else {
-				stepInfo->pRobotActions->addActionRedistribution(myInfo->L*0.01/*новое значение A*/, myInfo->L*0.79/*новое значение P*/, myInfo->L*0.20 /* новое значение V*/);
-			}
+			stepInfo->pRobotActions->addActionRedistribution(myInfo->L*0.01/*новое значение A*/, myInfo->L*0.79/*новое значение P*/, myInfo->L*0.20 /* новое значение V*/);
+		    stepInfo->pRobotActions->addActionMove(rand() % 5, rand() % 5);
+				
+			
 		}
 	}
 delete myInfo;
